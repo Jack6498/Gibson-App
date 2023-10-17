419006620b30316cedccdb94e73d254c11f566a1
Jack6498 <94179009+Jack6498@users.noreply.github.com>
2023-10-17 14:32:24 +0000

    connected to server

---
+++ phpFunctions/submitLoginCreds.php
@@ -0,0 +1,36 @@
+<?php
+
+$fullName = $_POST['fullName'];
+$newEmail = $_POST['newEmail'];
+$newPassword = $_POST['newPassword'];
+
+
+// I need to verify that these are the right credientals
+$servername = "192.185.16.86";
+$username = "wcsccsco_stu";
+$password = "Knights#23!!";
+$database = "userInfo";
+$port = 3306;
+
+// Create a MySQLi database connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+
+// Check the connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// SQL query to insert user data into a table (e.g., users)
+$sql = "INSERT INTO `userInfo`(`email`, `username`, `pwd`, `type`) VALUES ('$newEmail','$fullName','$newPassword','teacher')"
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+// Close the database connection
+$conn->close();
+
+?>
--- script.js
+++ script.js
@@ -20,10 +20,13 @@ choices.querySelectorAll('li').forEach(li => {
 //send data to server and check if it matches any existing users 
 function ProcessLogin() {
   //where I put my php function
+  console.log("ran processLogin")
+  //meets requirements should make sure email is <= 320 characters, the password is <=64 and the name is <= 256
   var meetsRequirements = true;
-  var password = document.getElementById('password').innerHTML;
-  var email = document.getElementById('email').innerHTML;
+  var password = document.getElementById('password').value;
+  var email = document.getElementById('email').value;
   if (meetsRequirements) {
+    console.log("in the if")
     window.location.href = "Dashboard/dashboard.html";
   }
 }
@@ -34,17 +37,53 @@ function ProcessSignUp() {
   //where I put my php function
   console.log("sign up")
   var meetsRequirements = true;
-  var password = document.getElementById('newPassword').innerHTML;
-  var confirmation = document.getElementById('confirmPassword').innerHTML;
-  var newEmail = document.getElementById('newEmail').innerHTML;
-  if (password == confirmation){
-    if (meetsRequirements) {
+  var password = document.getElementById('newPassword').value;
+  var confirmation = document.getElementById('confirmPassword').value;
+  var newEmail = document.getElementById('newEmail').value;
+  if (password == confirmation) {
+    if (checkInput(password)) {
       //probably need to show a verification prompt
-    window.location.href = "Dashboard/dashboard.html";
-  }else{
-      alert("Passwords must match")
+      window.location.href = "Dashboard/dashboard.html";
+    }
+    } else {
+      alert("Passwords must match");
   }
+}
+
+function checkInput(inputText) {
+  // Create an empty array to store the error messages.
+  const errors = [];
+
+  // Check the length of the input text.
+  if (inputText.length < 8) {
+    errors.push("The input text must be at least 8 characters long.");
+    alert("The input text must be at least 8 characters long.");
   }
+
+  // Check if the input text contains a capital letter.
+   const hasCapitalLetter = /[A-Z]/.test(inputText);
+  if (!hasCapitalLetter) {
+    errors.push("The input text must contain at least 1 capital letter.");
+    alert("The input text must contain at least 1 capital letter.");
+  }
+
+  // Check if the input text contains a symbol.
+  const hasSymbol = /[!@#$%^&*]/.test(inputText);
+  if (!hasSymbol) {
+    errors.push("The input text must contain at least 1 symbol.");
+    alert("The input text must contain at least 1 symbol.");
+  }
+
+  // Check if the input text contains a number.
+  const hasNumber = /[0-9]/.test(inputText);
+  if (!hasNumber) {
+    errors.push("The input text must contain at least 1 number.");
+    alert("The input text must contain at least 1 number.");
+  }
+
+    
+  // Return true if there are no errors, otherwise return false.
+  return errors.length === 0;
 }
 
 
@@ -63,19 +102,19 @@ Sign_In_Button.addEventListener('click', () => {
 });
 
 
-document.getElementById("runPHPButton").addEventListener("click", function() {
-  var xhr = new XMLHttpRequest();
-  xhr.open("GET", "phpFunctions/testing.php", true);
+// document.getElementById("runPHPButton").addEventListener("click", function() {
+//   var xhr = new XMLHttpRequest();
+//   xhr.open("GET", "phpFunctions/testing.php", true);
 
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState == 4 && xhr.status == 200) {
-      var resultContainer = document.getElementById("resultContainer");
-      resultContainer.innerHTML = xhr.responseText;
-    }
-  };
+//   xhr.onreadystatechange = function() {
+//     if (xhr.readyState == 4 && xhr.status == 200) {
+//       var resultContainer = document.getElementById("resultContainer");
+//       resultContainer.innerHTML = xhr.responseText;
+//     }
+//   };
 
-  xhr.send();
-});
+//   xhr.send();
+// });
 
 
 
